09dd0ca5f6b1d865375b62cb3399eac67dedb50b
mwangiarnold0@gmail.com <mwangiarnold0@gmail.com>
2022-10-27 01:15:47 -0700

    initial commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Online Ticket Reservation System';
+//E-Ticketing System For Railway
+$supervisor_name = "Austin nganga";
+$developer_name = "Becky njeri";
+$developer_matric = "18/52HA022";
